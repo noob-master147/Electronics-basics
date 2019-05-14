2507412ca57bc17396eb089c767e89cabd83a7bd
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-15 01:22:48 +0530

    action made smoother

--- servo_check/servo_check.ino
+++ servo_check/servo_check.ino
@@ -15,20 +15,20 @@ void setup() {
 
 void loop() {
   int i=0;
-  for(i=0;i<180;i++)
+  for(i=30;i<180;i+=2)
   {
     s1.write(i);
     s2.write(i);
-    delay(3);
+    delay(5);
   }
-  delay(2);
+  delay(200);
 
-  for(i=180;i>0;i--)
+  for(i=180;i>30;i-=2)
   {
     s1.write(i);
     s2.write(i);
-    delay(3); 
+    delay(5); 
   }
-  delay(2);
+  delay(200);
 
 }
