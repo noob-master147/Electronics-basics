c78382b4c4804472712f76e743ef07b873530189
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-15 23:48:44 +0530

    connected eveything

--- joystick_servo/joystick_servo.ino
+++ joystick_servo/joystick_servo.ino
@@ -1,10 +1,15 @@
+#include<Servo.h>
 
+Servo sx;
+Servo sy;
 void setup() {
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   Serial.begin(9600);
+  sx.attach(8);
+  sy.attach(9);
 
 }
 
@@ -12,9 +17,13 @@ void loop() {
   int x=analogRead(A1);
   int y=analogRead(A2);
 
-  int dx = map(x,0,1024,0,180);
-  int dy = map(y,0,1024,0,180);
+  int dx = map(x,0,1024,30,180);
+  int dy = map(y,0,1024,30,180);
   Serial.println(dx);
   Serial.println(dy);
+
+  sx.write(dx);
+  sy.write(dy);
+  delay(5);
 
 }
