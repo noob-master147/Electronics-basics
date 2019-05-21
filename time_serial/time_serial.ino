f74a977ef424ef917800b328b2ac2613222cb66a
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-21 17:54:48 +0530

    project Terminated

--- time_serial/time_serial.ino
+++ time_serial/time_serial.ino
@@ -1,22 +1,41 @@
-int time;
-int prev = 0;
+int time;                     //keeps track of time for 50 days
+int prev = 0;                 //kepps track of time after reset
 void setup() {
-  pinMode(2,INPUT);
+  pinMode(10,OUTPUT);         //Connected to Reset pin
+  pinMode(2,INPUT);           //Input from External trigger
   Serial.begin(9600);
 }
 void loop() {
   
-  //Serial.print("Time: ");
-  time = millis()/1000;
   
-  //Serial.println(time); //prints time since program started
+  time = millis()/1000;
+  int state = digitalRead(2);
+  digitalWrite(10, LOW);
+  if(state)                   // if button is true then reset
+  {
+    digitalWrite(10, HIGH);   // Set Reset pin to LOw to Restart the Arduino
+    delay(100);
+    digitalWrite(10, LOW);    // Return back the reset to High
+  }
+  
+  
   if(prev != time)
   {
     prev+=1;
+    Serial.print(state);
+    Serial.print(" ");
     Serial.println(prev);
   }
   if(prev>=1000)
   {
     Serial.println("TIME UP");
+    delay(5000);
+    Serial.println("RESET TIME");
+    digitalWrite(10, HIGH);
+    delay(100);
+    digitalWrite(10, LOW);
+    
   }
+  
+
 }
