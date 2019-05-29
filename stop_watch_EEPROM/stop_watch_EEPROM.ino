d87a48d6419f40846d28b519b4e277034d58566b
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-29 16:30:44 +0530

    stop watch

--- stop_watch_EEPROM/stop_watch_EEPROM.ino
+++ stop_watch_EEPROM/stop_watch_EEPROM.ino
@@ -1,53 +1,35 @@
+
+
 #include <EEPROM.h>
- 
-#define SAMPLE_TIME 1000  //The time between each EEPROM write function call in ms
- 
 
-int address = 0;      //EEPROM address counter
+int time;                     //keeps track of time for 50 days
 int prev = 0;
-unsigned long timer;
- 
 
-
-void writeTemp();
- 
-void setup(){
-  Serial.begin(9600);     //start the serial connection as always
-  timer = millis()/1000;         //millis() returns the time since program start in ms
-  pinMode(2,INPUT);
+void setup() {
+  Serial.begin(9600);
+  EEPROM.get(0, prev);     
 }
- 
-void loop(){
-  if(millis()/1000-timer > SAMPLE_TIME)  //check if it's time to do a temp sensor sample
+void loop() {
+  
+  
+  time = millis()/1000;
+  EEPROM.update(0, prev);
+  
+  if(prev != time)
   {
-    Serial.println("TIME UP!");
+    prev+=1;
+    EEPROM.put(0, prev);
+    delay(996);
+    Serial.println(prev);
+  }
+  if(prev>=1000)
+  {
+    Serial.println("TIME UP");
+    delay(5000);
+    for (int i = 0 ; i < EEPROM.length() ; i++) 
+    EEPROM.write(i, 0);
     
   }
- 
-}
- 
-
- 
-
- 
-void writeTemp()
-{
   
-   
-  EEPROM.update(address, timer);         //write value to current address counter address
- 
-  Serial.print("Sensor value stored at address ");
-  Serial.println(address);
-   
-  
+
 }
-
-
-
-/*  NOTES
- *  An EEPROM write takes 3.3 ms to complete.
- * The EEPROM memory has a specified life of 100,000 write/erase cycles, 
- * so you may need to be careful about how often you write to it.
- * so using update() function instead of write() 
- * can save cycles if the written data does not change often
- */
