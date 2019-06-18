b9d01bc47eb089bb9bb06b7f9260dd2b6b6461f3
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-06-18 11:56:24 +0530

    parsing JSON object

--- arduino_JSON/arduino_JSON.ino
+++ arduino_JSON/arduino_JSON.ino
@@ -1,11 +1,18 @@
 #include <Arduino_JSON.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  const size_t capacity = JSON_OBJECT_SIZE(2) + 20;
+  DynamicJsonBuffer jsonBuffer(capacity);
 
+  const char* json = "{\"x\":12,\"y\":10}";
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  const char* json = "{\"x\":12,\"y\":10}";
+  JsonObject& root = jsonBuffer.parseObject(json);
 
+  int x = root["x"]; // x-coordinate
+  int y = root["y"]; // y-coordinate
 }
