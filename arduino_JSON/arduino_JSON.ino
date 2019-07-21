6e6a02843d2e44bd73d6f0cac7ea9a3715e60eda
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-07-21 19:34:19 +0530

    arduino json

--- arduino_JSON/arduino_JSON.ino
+++ arduino_JSON/arduino_JSON.ino
@@ -3,16 +3,20 @@
 void setup() {
   const size_t capacity = JSON_OBJECT_SIZE(2) + 20;
   DynamicJsonBuffer jsonBuffer(capacity);
-
-  const char* json = "{\"x\":12,\"y\":10}";
-
-  
+  Serial.begin(9600);
+    
 }
 
 void loop() {
-  const char* json = "{\"x\":12,\"y\":10}";
+  while(Serial.available() == 0){}
+  
+  const char* json = Serial.read();
   JsonObject& root = jsonBuffer.parseObject(json);
 
   int x = root["x"]; // x-coordinate
   int y = root["y"]; // y-coordinate
+  Serial.println(x);
+  Serial.println(y);
 }
+
+//"{\"x\":12,\"y\":10}"
